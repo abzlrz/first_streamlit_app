d411c1f6fc8458ec8a25187cb9ce778d44d73ba9
lawrence <30492113+abzlrz@users.noreply.github.com>
2023-04-19 21:39:57 +0800

    Update streamlit_app.py
    Fix the formats

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.header('Kale, Spinach & Rocket Smoothie')
-streamlit.header('Hard-boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
